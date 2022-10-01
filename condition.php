cc8f67b9b22ab39f2d7a7100ee689d24e5c0f5a3
Sudiptaguru <64124103+Sudiptaguru@users.noreply.github.com>
2022-10-01 16:00:49 +0530

    Add files via upload

---
+++ condition.php
@@ -0,0 +1,11 @@
+<h1>Terms and Conditions</h1>
+<?php
+$data = "Feeling unsafe in current company and got a new job opportunity";
+$data1 = "Continue only with one company";
+if($data){
+	echo "1. Continue with current company and new company together"."<br>";
+}
+if($data1){
+	echo "2. Leave the current company and continue with new company";
+}
+?>

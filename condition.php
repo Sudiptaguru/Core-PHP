a0a3123db65aaf5ae5edc2a48292dcfb21527992
Sudiptaguru <64124103+Sudiptaguru@users.noreply.github.com>
2022-10-01 16:03:47 +0530

    Update condition.php

--- condition.php
+++ condition.php
@@ -3,7 +3,7 @@
 $data = "Feeling unsafe in current company and got a new job opportunity";
 $data1 = "Continue only with one company";
 if($data){
-	echo "1. Continue with current company and new company together"."<br>";
+	echo "1. Continue with current company and new company together including Saturday and Sunday in current company"."<br>";
 }
 if($data1){
 	echo "2. Leave the current company and continue with new company";
